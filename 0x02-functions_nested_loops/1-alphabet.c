1071e1873ea1b20e28893b7969f54a39bc855fd9
Peterpilex <afolamipeter0002@gmail.com>
2022-07-05 19:34:57 -0700

    0x02 Functions

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,16 @@
 #include "main.h"
-
 /**
- *  * print_alphabet - entry point
- *   * Return: 0 Success
- *    */
+ * print_alphabet - entry point
+ * Return: 0 Success
+ */
 void print_alphabet(void)
 {
-		char j;
+char j;
 
+for (j = 'a'; j <= 'z'; j++)
+{
+  _putchar(j);
+}
+_putchar('\n');
+}
+		
