4ce2942fb199c07283b44ac6febddc76a194489a
Peterpilex <afolamipeter0002@gmail.com>
2022-07-05 18:34:24 -0700

    0x02 Functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,17 +5,17 @@
  * Description: print _putchar followed on a new line
  * Return: always 0
  */
-   int main(void)
+int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
-	_putchar('\n');
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 
-        return (0);
+return (0);
 } 
